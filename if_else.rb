c080be0b68b9b9ac560cf18b60ebb40378f2c56f
Carla Bravo <carlabravo@id.uff.br>
2023-02-10 10:01:35 -0300

    Add if_else.rb

---
+++ if_else.rb
@@ -0,0 +1,15 @@
+# if condition
+#     # code to execute if condition is true
+#   else
+#     # code to execute if condition is false
+#   end
+
+  x = 10
+
+if x > 5
+  puts "x é maior que 5"
+else
+  puts "x é menor ou igual a 5"
+end
+
+# Output: x é maior que 5
