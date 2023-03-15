fcf261491c5760fb881d85bbe8834f75af37a394
Carla Bravo <carlabravo@id.uff.br>
2023-03-15 11:36:37 -0300

    Add exercise impar e par with while

---
+++ reforco_exercicios/impares_1_20_while.rb
@@ -0,0 +1,9 @@
+#Escreva um programa que imprima os números ímpares de 1 a 20 usando um loop do while.
+
+counter = 1
+while counter <= 20 do
+  if counter % 2 != 0
+  puts "O número #{counter} é ímpar"
+end
+  counter += 1
+end
