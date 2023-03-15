1ee64cf35bcc95ce7409d275f805bcb937590016
Carla Bravo <carlabravo@id.uff.br>
2023-03-15 09:47:51 -0300

    Add new exercise pares 1 a 20 laco while

---
+++ reforco_exercicios/pares_1_20_while.rb
@@ -0,0 +1,12 @@
+# Escreva um programa que imprima os números pares de 1 a 20 usando um loop while.
+
+counter = 1
+
+while counter <= 20 do
+  if counter % 2 == 0
+    puts "#{counter} é par"
+  end
+    counter += 1
+end
+
+
